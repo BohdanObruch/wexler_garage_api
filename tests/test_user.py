902f7748bcd2ab3a188c961eb51566035b24c745
BohdanObruch <bodan1992@gmail.com>
2023-11-01 10:32:20 +0300

    added testomat.io mark id tests

--- tests/test_user.py
+++ tests/test_user.py
@@ -7,14 +7,14 @@ from garage_api.schemas.garage import user_login, refresh_token
 
 class TestUser:
 
-    @mark.testomatio('@Tdd9585c3')
+    @mark.testomatio('@T938dc941')
     def test_user_login(self):
         payload = {'username': username, 'password': password}
         response = garage_authorization().post(f'/user/login/', data=payload)
         assert response.status_code == 200
         assert S(user_login) == response.json()
 
-    @mark.testomatio('@Te07535ce')
+    @mark.testomatio('@Ta35d180c')
     def test_login_refreh(self, token):
         data = {'refresh': token[1]}
         response = garage_authorization().post(f'/user/login/refresh/', data=data)
