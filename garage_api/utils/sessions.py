3b20fa8e49e41dc6043fcac8c6f48756853be95a
BohdanObruch <bodan1992@gmail.com>
2023-11-06 01:09:50 +0300

    fix import env data

--- garage_api/utils/sessions.py
+++ garage_api/utils/sessions.py
@@ -1,15 +1,12 @@
 import os
 from garage_api.utils.requests_helper import BaseSession
-from dotenv import dotenv_values
-
-dotenv = dotenv_values()
 
 
 def garage_authorization() -> BaseSession:
-    api_authorization = dotenv.get('API_AUTHORIZATION')
+    api_authorization = os.getenv('API_AUTHORIZATION')
     return BaseSession(base_url=api_authorization)
 
 
 def garage() -> BaseSession:
-    api_url = dotenv.get('API_URL')
+    api_url = os.getenv('API_URL')
     return BaseSession(base_url=api_url)
